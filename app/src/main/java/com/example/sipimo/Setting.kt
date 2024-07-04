af6c6087656e5e88cca9d83d05764e9d38c21b5a
putra_swastika <iketutputraswastika@gmail.com>
2024-07-04 09:37:19 +0800

    ini udah bisa button log out

--- app/src/main/java/com/example/sipimo/Setting.kt
+++ app/src/main/java/com/example/sipimo/Setting.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import com.google.firebase.auth.FirebaseAuth
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,6 +25,8 @@ class Setting : Fragment() {
     private var param2: String? = null
 
     private lateinit var userBtn: Button
+    private lateinit var logoutBtn: Button
+    private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +34,8 @@ class Setting : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
+        mAuth = FirebaseAuth.getInstance()
     }
 
     override fun onCreateView(
@@ -41,14 +46,27 @@ class Setting : Fragment() {
         val view = inflater.inflate(R.layout.fragment_setting, container, false)
 
         userBtn = view.findViewById(R.id.userButton)
+        logoutBtn = view.findViewById(R.id.logout) // Pastikan ID ini sesuai dengan yang di XML
 
         userBtn.setOnClickListener {
             val intent = Intent(requireContext(), user_setting::class.java)
             startActivity(intent)
+        }
 
+        logoutBtn.setOnClickListener {
+            mAuth.signOut() // Lakukan logout dari Firebase
+            sendToLandingPage() // Panggil fungsi untuk kembali ke halaman login atau halaman awal aplikasi
         }
 
         return view
+    }
+
+    private fun sendToLandingPage() {
+        // To send user to Landing Page or Login Page
+        val landingIntent = Intent(requireContext(), LandingPage::class.java)
+        landingIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(landingIntent)
+        requireActivity().finish()
     }
 
     companion object {
